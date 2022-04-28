ad4af12c5b716fae713f71b1b93d297c1e17057a
feldblume5263 <feldblume5263@gmail.com>
2022-04-28 17:58:22 +0900

    feat: init project

---
+++ PASTArt/PASTArt/PASTArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PASTArtApp.swift
+//  PASTArt
+//
+//  Created by Noah Park on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct PASTArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
